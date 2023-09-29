18ef9d4fab60b6184ebf67a2ac7c022509ca964a
Kelley van Evert <hello@klve.nl>
2023-09-29 14:34:11 +0200

    added og metadata

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -5,12 +5,29 @@ import "./styles.css";
 
 import { Layout } from "../layouts/Layout";
 
+import chen1small from "../assets/chen1small.jpg";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Kelley van Evert</title>
+        <meta property="og:title" content="Kelley van Evert" />
+        <meta
+          property="og:description"
+          content="Kelley van Evert's personal site"
+        />
+        <meta
+          property="description"
+          content="Kelley van Evert's personal site"
+        />
+        <meta property="og:type" content="profile" />
+        <meta property="og:url" content="https://klve.nl/" />
+        <meta property="og:image" content={chen1small.src} />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content={"" + chen1small.width} />
+        <meta property="og:image:height" content={"" + chen1small.height} />
       </Head>
       <Component {...pageProps} />
     </Layout>
