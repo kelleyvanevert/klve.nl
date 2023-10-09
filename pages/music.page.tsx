8015cf03dc6f562326cd78c78d2307efd0016ede
Kelley van Evert <kelley@ijsfontein.nl>
2023-10-09 09:57:58 +0200

    removed rocktober musics

--- pages/music.page.tsx
+++ pages/music.page.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React from "react";
 import dynamic from "next/dynamic";
 import { GetStaticProps } from "next";
 import { executeQraphQLRequest } from "lib/graphql";
@@ -54,8 +54,6 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 };
 
 export default function Page({ tracks }: Props) {
-  const [rock, setRock] = useState(false);
-
   return (
     <section className="wrap relative mt-[60px] md:mt-[100px]">
       <h1 className="hidden">Music</h1>
@@ -86,67 +84,6 @@ export default function Page({ tracks }: Props) {
           />
         );
       })}
-
-      <h2>Rocktober 2023</h2>
-      <p className="relative">
-        Let's see if anything good comes from this... (
-        <button className="link" onClick={() => setRock((b) => !b)}>
-          toggle
-        </button>
-        )
-      </p>
-
-      {rock && (
-        <Fragment>
-          <AudioPlayer
-            className="mt-12"
-            title="Day 07 - Rust, a memory-safe programming language ft. Auke"
-            url="https://cdn.jsdelivr.net/gh/kelleyvanevert/rocktober2023/07 - Rust, a memory-safe programming language ft. Auke.wav"
-          />
-
-          <AudioPlayer
-            className="mt-12"
-            title="Day 08 - Powerbank"
-            url="https://cdn.jsdelivr.net/gh/kelleyvanevert/rocktober2023/08 - Powerbank.wav"
-          />
-
-          <AudioPlayer
-            className="mt-12"
-            title="Day 06 - Exhausted"
-            url="https://cdn.jsdelivr.net/gh/kelleyvanevert/rocktober2023/06 - Exhausted.wav"
-          />
-
-          <AudioPlayer
-            className="mt-12"
-            title="Day 05 - Scary story"
-            url="https://cdn.jsdelivr.net/gh/kelleyvanevert/rocktober2023/05 - Scary story.wav"
-          />
-
-          <AudioPlayer
-            className="mt-12"
-            title="Day 04 - Breathe"
-            url="https://cdn.jsdelivr.net/gh/kelleyvanevert/rocktober2023/04 - Breathe.wav"
-          />
-
-          <AudioPlayer
-            className="mt-12"
-            title="Day 03 - Kort geding"
-            url="https://cdn.jsdelivr.net/gh/kelleyvanevert/rocktober2023/03 - Kort geding.wav"
-          />
-
-          <AudioPlayer
-            className="mt-12"
-            title="Day 02 - Love is blind"
-            url="https://cdn.jsdelivr.net/gh/kelleyvanevert/rocktober2023/02 - Love is blind.wav"
-          />
-
-          <AudioPlayer
-            className="mt-12"
-            title="Day 01 - Carlota explores the ocean"
-            url="https://cdn.jsdelivr.net/gh/kelleyvanevert/rocktober2023/01 - Carlota explores the ocean.wav"
-          />
-        </Fragment>
-      )}
     </section>
   );
 }
