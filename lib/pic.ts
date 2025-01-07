9b4ec1474ff0311ab8d82d4cecd8c5b79c007299
Kelley van Evert <kelley@ijsfontein.nl>
2025-01-07 22:27:07 +0100

    changed pic

--- lib/pic.ts
+++ lib/pic.ts
@@ -1,7 +1,11 @@
 // goofy, wearing yellow + sunglasses in park
-export { default as pic } from "../assets/_DSC0995.jpg";
+// export { default as pic } from "../assets/_DSC0995.jpg";
 
 // contemplative, in the train
 // export { default as pic } from "../assets/_DSC1063.jpg";
 
-// export { default as pic } from "../assets/_DSC1140.jpg";
+// goodlooking, looking straight in the camera, vacation pic
+export { default as pic } from "../assets/_DSC1140.jpg";
+
+// silly, black and white, having fun
+// export { default as pic } from "../assets/_DSC0720.jpg";
