c6bdc6e4d458d3d43930c1e926cac20efa9ca46c
Kelley van Evert <kelley@ijsfontein.nl>
2026-07-08 09:02:47 +0200

    updated jizhu privacy statement

--- app/privacy-policy/[app]/page.tsx
+++ app/privacy-policy/[app]/page.tsx
@@ -2,23 +2,38 @@ import { notFound } from "next/navigation";
 
 type AppInfo = {
   title: string;
+  texts: string[];
 };
+
+const defaultTexts = [
+  "Our privacy policy is really simple: we don't collect or store any of your data, or track your usage in any way.",
+  "This app is just a little coding experiment. Your data stays on your phone.",
+];
 
 const APPS: Record<string, AppInfo> = {
   "origami-pixels": {
     title: "Origami Pixels",
+    texts: defaultTexts,
   },
   "arabic-words": {
     title: "العربية",
+    texts: defaultTexts,
   },
   "perry-16": {
     title: "Perry's 16",
+    texts: defaultTexts,
   },
   hexastack: {
     title: "Hexastack",
+    texts: defaultTexts,
   },
   jizhu: {
     title: "JiZhu",
+    texts: [
+      "My privacy policy is really simple: the app doesn't have user accounts or ask for any personal data, so I can't store or track it either.",
+      "I do track crashes though, to improve the app. But you can turn that off in the settings screen.",
+      "This app is just a little coding experiment, I have no financial or other evil incentives :)",
+    ],
   },
 };
 
@@ -43,14 +58,9 @@ export default async function PrivacyPolicyPage({ params }: Props) {
       <h1>Privacy Policy for {info.title}</h1>
 
       <div style={{ fontSize: "130%", lineHeight: "150%" }}>
-        <p>
-          Our privacy policy is really simple: we don't collect or store any of
-          your data, or track your usage in any way.
-        </p>
-        <p>
-          This app is just a little coding experiment. Your data stays on your
-          phone.
-        </p>
+        {info.texts.map((text, i) => {
+          return <p key={i}>{text}</p>;
+        })}
       </div>
     </div>
   );
